0b84430b43ebbf9bb84fd2eb5b72412f8936932c
gustavo2025001 <gustavobarbosagustavodepaulaba@gmail.com>
2026-09-15 02:48:57 -0500

    Add files via upload

---
+++ site-config.js
@@ -0,0 +1,8 @@
+// BOT AFILIADOS - configuração pública de rotas
+// Para futuras atualizações, altere as rotas aqui em vez de usar ../ ou caminhos que saiam do repositório.
+window.BOT_AFILIADOS_CONFIG = Object.freeze({
+  basePath: "/bot-afiliados/",
+  botUrl: "https://gustavo2025001.github.io/bot-afiliados/#bot",
+  storefrontPath: "/bot-afiliados/ofertas/",
+  version: "5.4.0"
+});
